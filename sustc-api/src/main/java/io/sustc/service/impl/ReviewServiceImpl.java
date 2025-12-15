07e443206b10b12085ac08464168be4767bcb4c2
咒之师 <18163520552@163.com>
2025-12-15 14:43:39 +0800

    初始化

---
+++ sustc-api/src/main/java/io/sustc/service/impl/ReviewServiceImpl.java
@@ -0,0 +1,44 @@
+package io.sustc.service.impl;
+
+import io.sustc.dto.AuthInfo;
+import io.sustc.dto.PageResult;
+import io.sustc.dto.RecipeRecord;
+import io.sustc.dto.ReviewRecord;
+import io.sustc.service.ReviewService;
+
+public class ReviewServiceImpl implements ReviewService {
+    @Override
+    public long addReview(AuthInfo auth, long recipeId, int rating, String review) {
+
+    }
+
+    @Override
+    public void editReview(AuthInfo auth, long recipeId, long reviewId, int rating, String review) {
+
+    }
+
+    @Override
+    public void deleteReview(AuthInfo auth, long recipeId, long reviewId) {
+
+    }
+
+    @Override
+    public long likeReview(AuthInfo auth, long reviewId) {
+        return 0;
+    }
+
+    @Override
+    public long unlikeReview(AuthInfo auth, long reviewId) {
+        return 0;
+    }
+
+    @Override
+    public PageResult<ReviewRecord> listByRecipe(long recipeId, int page, int size, String sort) {
+        return null;
+    }
+
+    @Override
+    public RecipeRecord refreshRecipeAggregatedRating(long recipeId) {
+        return null;
+    }
+}
